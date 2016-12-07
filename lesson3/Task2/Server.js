d18fb5895dba647ac9dc5aa858ef5ef2d1c5a07c
GREY25 <GREY25@ptc.local>
2016-12-07 13:55:26 +0200

    unfinished 7

--- lesson3/Task2/Server.js
+++ lesson3/Task2/Server.js
@@ -4,6 +4,20 @@ var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+var ObjectID = require('mongodb').ObjectID;
+
+var db = new Db('tutor',
+                new Server('localhost', 27017, {safe: true},
+                    {auto_reconnect: true}, {}));
+
+db.open(function () {
+    console.log('Mongo DB is opened');
+    db.collection('notes', function (error, notes) {
+        db.notes = notes;
+    });
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,37 +31,35 @@ app.use(session({
 app.listen(3000);
 
 app.get('/notes', function (req, res) {
-    fs.readFile('notes.json', function(err, result){
-        if (result) {
-            result = '' + result;
-            result = result.substring(0, result.length - 1);
-            result = '[' + result + ']';
-            result = result.split('\n').join(',');
-            res.send(result);
-        } else {
-            res.end();
-        }
+    var sortOrder = req.body.params.orderBy;
+    db.notes.find(req.query).sort({sortOrder: 1}).toArray(function (err, items) {
+        res.send(items);
     });
 });
 
 app.post('/notes', function (req, res) {
-    var note = req.body;
-    var noteText = JSON.stringify(note) + '\n';
-    fs.appendFile('notes.json', noteText, function (err) {
-        if (err) console.log('Smth wrong');
-        res.end();
-    });
+    db.notes.insert(req.body);
+    res.end();
 });
 
 app.delete('/notes', function (req, res) {
-    var id = req.query.id;
-    var notes = req.session.notes||[];
-    var updatedNotesList = [];
-    for (var i = 0; i < notes.length; i++) {
-        if (notes[i].id != id) {
-            updatedNotesList.push(notes[i]);
-        }
+   var id = new ObjectID(req.query.id);
+   db.notes.remove({_id: id}, function (err) {
+       if (err) {
+            console.log(err);
+            res.send('Failed');
+       } else {
+            res.send('Success');
+       }
+   });
+});
+
+app.put('/notes', function (req, res) {
+    var orderID = req.body.params.orderID;
+    console.log(req.body.params);
+    if (orderID != 0) {
+        db.notes.update({"orderID": orderID}, {$set: {orderID: orderID - 1}});
+        db.notes.update({"orderID": orderID - 1}, {$set: {orderID: orderID}});
     }
-    req.session.notes = updatedNotesList;
     res.end();
 });
--- lesson3/Task2/public/notes.js
+++ lesson3/Task2/public/notes.js
@@ -2,9 +2,10 @@ var module = angular.module('myapp', []);
 
 module.controller('NotesController', function ($scope, $http) {
     $scope.notes = [];
+    $scope.orderID = 0;
     
     var update = function () {
-        $http.get('/notes')
+        $http.get('/notes', {params: {orderBy: $scope.orderSort.value}})
             .success(function (notes) {
                 $scope.notes = notes;
             });
@@ -13,12 +14,16 @@ module.controller('NotesController', function ($scope, $http) {
     update();
     
     $scope.add = function () {
+        $scope.noteDate = new Date().getTime();
         var note = {
-            text: $scope.text
+            text: $scope.text,
+            date: $scope.noteDate,
+            orderID: $scope.orderID
         };
         $http.post('/notes', note)
             .success(function () {
                 $scope.text = '';
+                $scope.orderID += 1;
                 update();
             });
     };
@@ -26,7 +31,16 @@ module.controller('NotesController', function ($scope, $http) {
     $scope.remove = function (id) {
         $http.delete('/notes', {params: {id: id}})
             .success(function () {
+                $scope.orderID -= 1;
                 update();
             });
-    }
+    };
+
+    $scope.sendToTop = function (orderID) {
+        $http.put('/notes', {params: {orderID: orderID}})
+            .success(function () {
+                update();
+            });
+    };
+
 });
